52a1bf6c15f9a453151a146c86a796826cd60b9b
Alexander <Alexander29_90@icloud.com>
2024-07-25 14:47:03 +0500

    Added SettingsViewController

---
+++ Randomizer/SettingsViewController.swift
@@ -0,0 +1,22 @@
+//
+//  SettingsViewController.swift
+//  Randomizer
+//
+//  Created by Александр on 25.07.2024.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    @IBOutlet var minimumValueTF: UITextField!
+    @IBOutlet var maximumValueTF: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func cancelButtonPressed() {
+        
+    }
+}

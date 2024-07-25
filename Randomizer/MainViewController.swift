11fe830b6d63ded00bd0312aaef2b1578e69a307
Alexander <Alexander29_90@icloud.com>
2024-07-25 13:50:16 +0500

    Linked UI elements to code

--- Randomizer/MainViewController.swift
+++ Randomizer/MainViewController.swift
@@ -9,11 +9,23 @@ import UIKit
 
 class MainViewController: UIViewController {
 
+    @IBOutlet var minimumValueLabel: UILabel!
+    @IBOutlet var maximumValueLabel: UILabel!
+    
+    @IBOutlet var randomValueLabel: UILabel!
+    
+    @IBOutlet var getRandomNumberButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        getRandomNumberButton.layer.cornerRadius = 15
     }
 
-
+    @IBAction func getRundomNumberDidPressed() {
+        
+        
+    }
+    
 }
 
